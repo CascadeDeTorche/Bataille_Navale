78b11628bd903bf2c834aafdc9eb4b536bcf64c7
CascadeDeTorche <niddam.ethan@gmail.com>
2020-06-05 15:10:44 +0200

    Demande du nombre de joueur

--- main.cpp
+++ main.cpp
@@ -6,7 +6,14 @@ int main()
 {
     int nbjoueur=0;
     cout << "Bonjour et bievenue dans la bataille navale" << endl;
-    cout <<"Voulez -vous jouer a 1 ou 2 joueurs :"<<;
+    cout <<"Voulez-vous jouer a 1 ou 2 joueurs :";
+    cin >>nbjoueur;
+    while(nbjoueur>2 ||nbjoueur<1)
+    {
+        cout<<"Nombre de joueurs invalide"<<endl;
+        cout <<"Voulez-vous jouer a 1 ou 2 joueurs :";
+        cin >>nbjoueur;
+    }
 
 
 
